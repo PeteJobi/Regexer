bf776d11c4c226160f3e8aaa848e05a35a7364d0
PeteJobi <masterpdon@gmail.com>
2024-06-01 17:37:44 +0100

    Files are now read from executable directory

--- RegexerConsole/Program.cs
+++ RegexerConsole/Program.cs
@@ -1,25 +1,25 @@
 ﻿Console.WriteLine("Hello, World!");
 
-
-await using var fileStream = File.OpenRead("../../../../RegexerConsole/Input.tsx");
+await using var fileStream = File.OpenRead(Path.Combine(Environment.CurrentDirectory, "Input.txt"));
 {
     using var streamReader = new StreamReader(fileStream);
     var input = await streamReader.ReadToEndAsync();
-    await using var fileStream2 = File.OpenRead("../../../../RegexerConsole/Pattern.txt");
+    await using var fileStream2 = File.OpenRead(Path.Combine(Environment.CurrentDirectory, "Pattern.txt"));
     using var streamReader2 = new StreamReader(fileStream2);
     var pattern = await streamReader2.ReadToEndAsync();
-    await using var fileStream3 = File.OpenRead("../../../../RegexerConsole/Replace.txt");
+    await using var fileStream3 = File.OpenRead(Path.Combine(Environment.CurrentDirectory, "Replace.txt"));
     using var streamReader3 = new StreamReader(fileStream3);
     var replace = await streamReader3.ReadToEndAsync();
     try
     {
         var result = await new Regexer.Regexer().AutoRegex(input, pattern, replace);
         Console.WriteLine(result);
-    }catch(Exception e){
+    }
+    catch (Exception e)
+    {
         Console.WriteLine(e);
     }
-    return;
-    await using var fileStream4 = File.OpenWrite("../../../../RegexerConsole/Output.cs");
-    await using var streamWriter = new StreamWriter(fileStream4);
+    //await using var fileStream4 = File.OpenWrite(Path.Combine(Environment.CurrentDirectory, "Output.txt"));
+    //await using var streamWriter = new StreamWriter(fileStream4);
     //await streamWriter.WriteLineAsync(result);
 }
