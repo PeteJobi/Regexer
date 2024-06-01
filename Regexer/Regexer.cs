3cec8f2f338c25222305dd52254b2005f7f38360
PeteJobi <masterpdon@gmail.com>
2024-06-01 17:30:51 +0100

    Added support for unordered optional lines [[name|u|lineeee]]

--- Regexer/Regexer.cs
+++ Regexer/Regexer.cs
@@ -48,6 +48,7 @@ public class Regexer
         pattern = Regex.Replace(pattern, "\r\n", "\r\n([^\\S\\r\\n]+)?");
         pattern = Regex.Replace(pattern, @"(\\\[\\\[(\w+(\|\w+)?)\\\]\\\])", "[$2]");
         pattern = Regex.Replace(pattern, @"(\\\[\\\[(\w+)?\{(.+)\}\\\]\\\])", "[$2{$3}]");
+        pattern = Regex.Replace(pattern, @"(\\\[\\\[(\w+\|)?u\|([^\r\n]+?)\\\]\\\])", "[$2u|$3]");
         pattern = Regex.Replace(pattern, @"(?>[^\S\r\n]+)(?!\[\w+\|ml\])", @"[^\S\r\n]+");
         var multiLineGroups = Regex.Matches(pattern, @"\[(\w+)\|ml\]").Select(g => g.Groups[1].Value);
         if (multiLineGroups.Any())
@@ -65,7 +66,21 @@ public class Regexer
         pattern = Regex.Replace(pattern, @"\[(\w+)\|so\]", "(?<$1>[^\\S\\r\\n]+?)?");
         pattern = Regex.Replace(pattern, @"\[(\w+)\{(.+)\}\]", "(?<$1>$2)");
         pattern = Regex.Replace(pattern, @"\[\{(.+)\}\]", "$1");
-        bool replaceWasEmpty = replace == string.Empty;
+        var uMatches = Regex.Matches(pattern, @"(?<uLines>\r\n\(\[\^\\S\\r\\n\]\+\)\?(\[\^\\S\\r\\n\]\+)?\[(\w+\|)?u\|[^\r\n]+\])+");
+        //var bbb = uMatches[0].Groups["uLines"];
+        for (var i = uMatches.Count - 1; i >= 0; i--)
+        {
+            var uMatch = uMatches[i];
+            var lMatches = uMatch.Groups["uLines"].Captures.Select(c => Regex.Match(c.Value, @"\r\n.+?\[((?<uName>\w+)\|)?u\|(?<uLine>.+)\]"));
+            replace = lMatches.Select(m => m.Groups["uName"].Value).Aggregate(replace, (current, name) => current.Replace($"[[{name}]]", $"[[{name}|]]"));
+            IEnumerable<(string line, string name)> linesAndNames = lMatches.Select(m => (m.Groups["uLine"].Value, m.Groups["uName"].Value));
+            var inAnyOrder = string.Join(string.Empty, linesAndNames.Select(l => $"(?=.*({l.line})?)"));
+            var noDuplicates = $"(?!.*({string.Join('|', linesAndNames.Select(l => $"\\s+{l.line}"))})+.*\\1)";
+            var nothingElseBesidesThem = $"({string.Join('|', linesAndNames.Select(l => "\\s+" + (l.name == string.Empty ? l.line : $"(?<{l.name}>{l.line})")))})*";
+            var fullPattern = inAnyOrder + noDuplicates + nothingElseBesidesThem;
+            pattern = pattern[..uMatch.Index] + fullPattern + pattern[(uMatch.Index + uMatch.Length)..];
+        }
+
         replace = "${space}" + replace;
         replace = Regex.Replace(replace, "\r\n", "\r\n${space}");
         replace = Regex.Replace(replace, @"\[\[(\w+)\]\]", "${$1}");
@@ -99,6 +114,24 @@ public class Regexer
                 {
                     var match = postMatches.ElementAt(j);
                     var rep = string.Join("\r\n", lines.Select(line => match.Groups["space"].Value + match.Groups["before"] + line + match.Groups["after"])) + "\r\n";
+                    result = result[..match.Index] + rep + result[(match.Index + match.Length)..];
+                }
+            }
+        }
+
+        var uLineGroups = Regex.Matches(replace, @"\[\[(\w+)\|([^\r\n]+)?\]\]").Select(g => g.Groups[1].Value).Distinct();
+        foreach (var group in uLineGroups)
+        {
+            var uResultMatches = Regex.Matches(result, string.Format(@"(?<uSpace>\s+)\[\[{0}\|(?<uLine>[^\r\n]+)?\]\]", group));
+            for (var i = matches.Count - 1; i >= 0; i--)
+            {
+                var inputMatch = matches[i].Groups[group];
+                var segmentCount = uResultMatches.Count / matches.Count;
+                var jInit = segmentCount * (i + 1);
+                for (var j = jInit - 1; j >= jInit - segmentCount; j--)
+                {
+                    var match = uResultMatches.ElementAt(j);
+                    var rep = !inputMatch.Success ? string.Empty : match.Groups["uSpace"].Value + (match.Groups["uLine"].Success ? match.Groups["uLine"].Value : inputMatch.Value);
                     result = result[..match.Index] + rep + result[(match.Index + match.Length)..];
                 }
             }
